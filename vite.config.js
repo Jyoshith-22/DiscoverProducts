80f72a6ad8b6addaa81323de32e918b32697d61d
Uddandi Jyoshith Sai <itzjyoshith@gmail.com>
2026-07-15 11:01:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/DiscoverProducts/', // Make sure to include the leading and trailing slashes!
+  // ... rest of your config
 })
